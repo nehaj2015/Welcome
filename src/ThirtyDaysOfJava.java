f94b2a0c32100ad6219cde97cb7f021bd63cf864
Neha Joglekar <nehaj2015@users.noreply.github.com>
2022-05-08 16:45:32 +0530

    30 days, 3/10

--- src/ThirtyDaysOfJava.java
+++ src/ThirtyDaysOfJava.java
@@ -1,5 +1,7 @@
 import thirtydays.Person;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 /**
@@ -106,7 +108,6 @@ public class ThirtyDaysOfJava {
      * Given an int N, print its 1st 10 multiples
      * N x 1 = N ...
      */
-
     public static void day5() {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
@@ -115,5 +116,87 @@ public class ThirtyDaysOfJava {
             int b = N * i;
             System.out.printf("%d x %d = %d\n", N, i, b);
         }
+    }
+
+    /**
+     * Day 6: Let's Review
+     * Simple string manipulation
+     * Separate the given string into odd and even character
+     */
+    public static void day6() {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.nextLine();
+        for(int i=0;i<n;i++) {
+            String s1= scan.nextLine();
+            for(int j=0; j < s1.length(); j+=2)
+                System.out.print(s1.charAt(j));
+            System.out.print(' ');
+            for(int j=1; j < s1.length(); j+=2)
+                System.out.print(s1.charAt(j));
+            System.out.println();
+        }
+        scan.close();
+    }
+
+    /**
+     * Day 7: Arrays
+     * Given an array, print in reverse order as single line, space-separated
+     */
+    public static void day7(){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++){
+            arr[i] = in.nextInt();
+        }
+        in.close();
+        for(int i=n-1;i>=0;i--){
+            System.out.print(arr[i]);
+            System.out.print(' ');
+        }
+        System.out.println();
+    }
+
+    /**
+     * Day 8: Maps/Dictionary/Key-Value
+     * Store Key-Value as input
+     * Print result of query or not found
+     */
+    public static void day8(){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String,Long> phoneBook = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            long phone = in.nextLong();
+            phoneBook.put(name,phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            Long num = phoneBook.get(s);
+            if(num==null){
+                System.out.println("Not found");
+            } else{
+                System.out.println(s+"="+num);
+            }
+        }
+        in.close();
+    }
+
+    /**
+     * Day 9: Recursion
+     * Implement factorial using recursion
+     */
+    public static void day9(){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.close();
+        System.out.println(factorial(n));
+    }
+    private static int factorial(int n){
+        if(n < 1) throw new IllegalArgumentException("Input cannot be less than 1:"+n);
+        return (n == 1) ? 1 : n * factorial(n-1);
     }
 }
